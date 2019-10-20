a10366ab2134455ba52faaa67161b11ea7839c35
CalebJ2 <calebinvents@gmail.com>
2019-10-19 22:53:20 -0700

    Add Arduino and Teensyduino to install script
    #16

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+# This script was tested on Ubuntu 18.04.3 64bit X86
+
 # set up ROS melodic http://wiki.ros.org/melodic/Installation/Ubuntu
 # Set up sources.list
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
@@ -15,4 +17,28 @@ echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 # Install dependancies
 sudo apt -y install python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+# Install Arduino IDE
+ARDUINO_VER="arduino-1.8.10"
+ARDUINO_PLATFORM="linux64"
+# Download
+wget https://downloads.arduino.cc/$ARDUINO_VER-$ARDUINO_PLATFORM.tar.xz
+# Extract
+sudo mkdir /opt/arduino
+sudo tar xf $ARDUINO_VER-$ARDUINO_PLATFORM.tar.xz -C /opt/arduino
+# Install
+sudo /opt/arduino/$ARDUINO_VER/install.sh
+# Cleanup
+sudo rm $ARDUINO_VER-$ARDUINO_PLATFORM.tar.xz
+
+# Install Teensyduino arduino extension https://www.pjrc.com/teensy/td_download.html
+# Allows non-root users to use it?
+sudo cp 49-teensy.rules /etc/udev/rules.d/
+# Download
+wget https://www.pjrc.com/teensy/td_148/TeensyduinoInstall.linux64
+# Install
+echo "Click through the installer and set install location to /opt/arduino/$ARDUINO_VER"
+chmod +x TeensyduinoInstall.linux64
+sudo ./TeensyduinoInstall.linux64
+
 
