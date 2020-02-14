c609aea3651decb37ca85153021e467e5dab7c29
CalebJ2 <calebinvents@gmail.com>
2020-02-13 21:49:37 -0800

    Add librealsense to install script
    #36

--- install.sh
+++ install.sh
@@ -23,6 +23,11 @@ sudo apt -y install python-rosinstall python-rosinstall-generator python-wstool 
 sudo apt-get install ros-melodic-rosserial-arduino
 sudo apt-get install ros-melodic-rosserial
 
+# Install realsense sdk
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
+sudo apt-get install -y librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg
+
 # Install Arduino IDE
 ARDUINO_VER="arduino-1.8.10"
 ARDUINO_PLATFORM="linux64"
