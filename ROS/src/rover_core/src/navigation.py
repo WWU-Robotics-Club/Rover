1b7421fea6323bbdabb0106410dffd9614b7f1e7
khalilxl <khalilxl2@hotmail.com>
2020-03-12 17:05:31 -0700

    Uploading Rover Branch

--- ROS/src/rover_core/src/navigation.py
+++ ROS/src/rover_core/src/navigation.py
@@ -9,27 +9,13 @@ def callback(data):
     rospy.loginfo(rospy.get_caller_id, data.data)
 
 def velocity_pub():
-    pub = ros.Publisher('wheel_velocity', String, queue_size=10)
-    rospy.init_node('navigation', anonymous=True)
+    pub = ros.Publisher('/wheel_velocity', String, queue_size=10)
     rate = rospy.Rate(10)
+    pub.publish("1")
 
 def position_sub():
-    rospy.init_node('navigation', anonymous=True)
-    rospy.Subscriber("wheel_position", String, callback)
-
-    rospy.spin()
-
-''' def camerapos_sub(_):      
-    rospy.init_node('navigation', anonymous=True)
-    rospy.Subscriber("camera_position", String, callback)
-
-    rospy.spin() '''
-
-def web_sub():
-    rospy.init_node('navigation', anonymous=True)
-    rospy.Subscriber("/web_vel", Twist, callback)
-
-if __name__ == '__main__':
+    rospy.Subscriber("/wheel_position", String, callback)
+    
     pos1 = 5
     pos2 = 10
     pos3 = 10
@@ -38,3 +24,32 @@ if __name__ == '__main__':
     message = str(pos1) + "," + str(pos2) + "," + str(pos3) + "," + str(pos4)
     
     print(message)
+
+def
+
+''' def camerapos_sub(_):      
+    rospy.init_node('navigation', anonymous=True)
+    rospy.Subscriber("camera_position", String, callback)
+
+    '''
+
+def web_sub():
+    rospy.Subscriber("/web_vel", Twist, callback)
+
+if __name__ == '__main__':
+    
+    rospy.init_node('navigation', anonymous=True)
+    
+    velocity_pub()
+    position_sub()
+    
+    
+   
+
+    '''
+    rospy.get_param('pos').split(,)
+
+    '''
+
+    rospy.spin()
+    
